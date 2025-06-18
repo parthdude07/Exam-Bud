06d7f2494d53350b7151b22a08883d0324f26611
Parth Dudhe <parthdudhe06@gmail.com>
2025-06-18 14:34:48 +0530

    feat: added Tailwind config and UI for course list

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
-  plugins: [react()],
-  server: { host: true, port: 3001 }
+  plugins: [react(),tailwindcss()],
+  server: { host: true, port: 4000 }
 });
